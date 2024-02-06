82af32d8ef54c8d03905c452ad95a4b9cda87c9f
Roka1206 <102240998+Roka1206@users.noreply.github.com>
2024-02-06 19:44:11 +0100

    fase 1 terminada

--- SocialBook/src/main.js
+++ SocialBook/src/main.js
@@ -1,7 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './assets/bootstrap.min.css'
+import './assets/bootstrap.bundle.min.js'
+import 'material-icons/iconfont/material-icons.css'
 
 import App from './App.vue'
 import router from './router'
